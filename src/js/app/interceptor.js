38178305e4e0d7b0424f2ef3567e623716e537e6
Diego Damato <diego.damato@m2mrj.local>
2017-05-25 15:50:31 -0300

    interceptar token do frota

--- src/js/app/interceptor.js
+++ src/js/app/interceptor.js
@@ -8,42 +8,8 @@ class Interceptor{
     }
 
     request(config){
-        let token = self._pegaToken(self._window.location.href);
-        
-        if(token)
-            localStorage.token = token;
-        
         config.headers.Authorization = localStorage.token;
-
         return config;
-    }
-
-    _pegaToken(url){
-        let param = [];
-        let token = "";
-        let urlFragmentada = [];
-        let paramFragmentado = [];
-
-        if(url){
-            urlFragmentada = url.split("?");
-        }
-
-        if (urlFragmentada[1]){
-            paramFragmentado = urlFragmentada[1].split("&");
-        }
-
-        if(paramFragmentado){
-            paramFragmentado.forEach(parametro =>{
-                if(parametro.indexOf("token") != -1){
-                    param = parametro.split("=");
-                    token = param[1];
-                    return;
-                }
-            });
-        }
-
-        return token;
-
     }
 
     responseError(response){
