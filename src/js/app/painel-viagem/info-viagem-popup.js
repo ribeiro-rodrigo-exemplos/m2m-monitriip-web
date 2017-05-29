58a84cdcfc42731904da582c81f0527c939b30e3
Rodrigo Ribeiro <rodrigo.santos@m2msolutions.com.br>
2017-05-29 14:44:13 -0300

    tratando dados nulos

--- src/js/app/painel-viagem/info-viagem-popup.js
+++ src/js/app/painel-viagem/info-viagem-popup.js
@@ -1,9 +1,10 @@
 class InfoViagemPopup{
-    constructor(document,dateUtil,geoCoderHelper){
+    constructor(document,dateUtil,geoCoderHelper,numberUtil){
         this._dateUtil = dateUtil;
         this._geocoderHelper = geoCoderHelper;
         this._document = document[0];
         this._detalheDaViagem = {};
+        this._numberUtil = numberUtil;
     }
 
     exibirDetalhesDaViagem(detalheViagem){
@@ -47,10 +48,20 @@ class InfoViagemPopup{
         let dataHoraInicial = this._detalheDaViagem.dataHoraInicial;
         let dataHoraFinal = servicosDaViagem[servicosDaViagem.length-1].dataHoraFinal;
 
-        this._detalheDaViagem.dataHoraInicial = this._dateUtil.formatarDataHora(dataHoraInicial);
-        this._detalheDaViagem.dataHoraFinal = this._dateUtil.formatarDataHora(dataHoraFinal);
+        this._detalheDaViagem.dataHoraInicial = dataHoraInicial ? this._dateUtil.formatarDataHora(dataHoraInicial) : '';
+        this._detalheDaViagem.dataHoraFinal = dataHoraFinal ? this._dateUtil.formatarDataHora(dataHoraFinal) : '';
 
-        this._detalheDaViagem.duracaoTotalDaViagem = this._dateUtil.obterDuracao(dataHoraInicial,dataHoraFinal);
+        this._detalheDaViagem.duracaoTotalDaViagem = dataHoraInicial && dataHoraFinal ? this._dateUtil.obterDuracao(dataHoraInicial,dataHoraFinal) : '';
+        let duracaoEmMinutos = servicosDaViagem.reduce((total,servico) => total+servico.duracao ? servico.duracao : 0,0);
+        this._detalheDaViagem.duracaoEmMinutos = duracaoEmMinutos ? this._detalheDaViagem.duracaoEmMinutos : '';
+
+        let totalKm = servicosDaViagem.reduce((total,servico) => total+servico.totalKm,0);
+        this._detalheDaViagem.totalKm = totalKm ? this._numberUtil.formatarNumero(totalKm) : '';
+
+        let direcaoContinuaMaxima = servicosDaViagem.reduce((maior,servico) => 
+            servico.direcaoContinua.tempoMaximo > maior ? servico.direcaoContinua.tempoMaximo :maior,0); 
+
+        this._detalheDaViagem.direcaoContinuaMaxima = direcaoContinuaMaxima ? direcaoContinuaMaxima : '';
 
         this._detalheDaViagem.calculoVelocidades = this._calcularVelocidades(servicosDaViagem);
 
@@ -63,13 +74,18 @@ class InfoViagemPopup{
 
         let velocidades = servicos.map(servico => servico.localizacoes)
                                   .reduce((acc,localizacoes) => {
-                                      localizacoes.forEach(l => acc.push(l.velocidade));
+                                      localizacoes.forEach(l => {
+                                          if(typeof(l.velocidade) == typeof(Number))
+                                            acc.push(l.velocidade);
+                                      });
                                       return acc; 
                                   },[]);
 
-        calculoVelocidade.velocidadeMaxima = Math.max.apply(null,velocidades);
-        calculoVelocidade.velocidadeMinima = Math.min.apply(null,velocidades); 
-        calculoVelocidade.velocidadeMedia = velocidades.reduce((acumulador,velocidade) => acumulador+velocidade,0)/velocidades.length; 
+        if(velocidades.length){
+            calculoVelocidade.velocidadeMaxima = Math.max.apply(null,velocidades);
+            calculoVelocidade.velocidadeMinima = Math.min.apply(null,velocidades); 
+            calculoVelocidade.velocidadeMedia = velocidades.reduce((acumulador,velocidade) => acumulador+velocidade,0)/velocidades.length;
+        } 
 
         return calculoVelocidade; 
     }
@@ -86,7 +102,12 @@ class InfoViagemPopup{
     }
 }
 
-InfoViagemPopup.$inject = ['$document','DateUtil','GeocoderHelper'];
+InfoViagemPopup.$inject = [
+    '$document',
+    'DateUtil',
+    'GeocoderHelper',
+    'NumberUtil'
+];
 
 angular.module('monitriip-web')
         .service('InfoViagemPopup', InfoViagemPopup);
