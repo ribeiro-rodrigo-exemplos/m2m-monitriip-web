17959b70cddca6e8e2b93b29cbbcd82b4c3240c8
Rodrigo Ribeiro <rodrigo.santos@m2msolutions.com.br>
2017-05-31 18:15:23 -0300

    integracao inicial do popup

---
+++ src/js/app/models/parada.js
@@ -0,0 +1,21 @@
+class Parada{
+    constructor(properties){
+        this._properties = properties; 
+    }
+
+    get motivo(){
+        return this._properties.motivoParada ? this._properties.motivoParada.split('_').join(' ').toLowerCase() : '';
+    }
+
+    get dataHora(){
+        return this._properties.dataHora;
+    }
+
+    get latitude(){
+        return this._properties.localizacao ? this._properties.localizacao.coordinates[0] : null;
+    }
+
+    get longitude(){
+        return this._properties.localizacao ? this._properties.localizacao.coordinates[1] : null;
+    }
+}
