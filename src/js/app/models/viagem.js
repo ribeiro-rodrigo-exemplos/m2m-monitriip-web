57d08a26207d36c11f7a0bb0dc02a5e95beacf80
Diego Damato <diego.damato@m2mrj.local>
2017-06-02 15:46:25 -0300

    insercao do mapa no info viagem

--- src/js/app/models/viagem.js
+++ src/js/app/models/viagem.js
@@ -163,17 +163,21 @@ class Viagem{
                             return acc;
                         },[]);
         
+        
+        let totalPeriodos = this._periodos.length;
+        this._localizacaoInicial = `${this._periodos[0].localizacaoInicial.coordinates[0]}, ${this._periodos[0].localizacaoInicial.coordinates[1]}`;
+        this._localizacaoFinal = `${this._periodos[totalPeriodos - 1].localizacaoFinal.coordinates[0]}, ${this._periodos[totalPeriodos - 1].localizacaoFinal.coordinates[1]}`;
+        
+        this._coordenadasPercurso.push([this._periodos[0].localizacaoInicial.coordinates[0], this._periodos[0].localizacaoInicial.coordinates[1]]);
+
         this._periodos.map(periodo =>{
             periodo.localizacoes.forEach(local =>{
                 if(local && local.localizacao)
-                    this._coordenadasPercurso.push({'posicao' : `${local.localizacao.coordinates[1]}, ${local.localizacao.coordinates[0]}`});
+                    this._coordenadasPercurso.push([local.localizacao.coordinates[0], local.localizacao.coordinates[1]]);
             });
         });
 
-        let totalPeriodos = this._periodos.length;
-
-        this._localizacaoInicial = {'posicao' : `${this._periodos[0].localizacaoInicial.coordinates[0]}, ${this._periodos[0].localizacaoInicial.coordinates[1]}`};
-        this._localizacaoFinal = {'posicao' : `${this._periodos[totalPeriodos - 1].localizacaoFinal.coordinates[0]}, ${this._periodos[totalPeriodos - 1].localizacaoFinal.coordinates[1]}`};
+        this._coordenadasPercurso.push([this._periodos[totalPeriodos - 1].localizacaoFinal.coordinates[0], this._periodos[totalPeriodos - 1].localizacaoFinal.coordinates[1]]);
    
     }
 
